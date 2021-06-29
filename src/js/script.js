b4faff9c4e9f0d196bd58670521905cd0068185b
Jakub Marszalek <marszalek.m.jakub@gmail.com>
2021-06-29 21:50:02 +0200

    Added function to download HTML

--- src/js/script.js
+++ src/js/script.js
@@ -2,17 +2,36 @@ export const script = () => {
 
 const textArea = document.querySelector("#textArea");
 const buttons = document.querySelectorAll(".button");
+const variablesDiv = document.querySelector(".variables")
+const variableName = document.querySelector("#variableName");
 const visualizationDiv = document.querySelector("#visualizationDiv");
 const buttonsTable = [...buttons];
+
+
+
 buttonsTable.forEach(button => {
     button.addEventListener("click", function() {
-        if (button.id === "checkVisualization" ) {
+        if (button.id === "checkVisualizationButton" ) {
             visualizationDiv.innerHTML = textArea.value;
+        }
+        if (button.id === "addVariableButton" ) {
+
+        }
+        if (button.id === "downloadHtmlButton" ) {
+           downloadHtml("index.html.html",textArea.value);
         }
     })
 });
 
 
-
-
+function downloadHtml(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + 
+    encodeURIComponent(text));
+    element.setAttribute('download', filename);
+     element.style.display = 'none';
+     document.body.appendChild(element);
+     element.click();
+     document.body.removeChild(element);
+   }
 }
